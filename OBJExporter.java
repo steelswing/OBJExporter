9eedb4493af561e308e2671b39eccb10afbd1b98
STEELSWING <53971838+steelswing@users.noreply.github.com>
2023-12-27 08:13:01 +1000

    Update OBJExporter.java

--- OBJExporter.java
+++ OBJExporter.java
@@ -59,17 +59,27 @@ public class OBJExporter {
         public final String name;
         public final int[] indices;
         public final float[] vertices;
+        public final float[] normals;
+        public final float[] textureCoords;
 
         public OBJMesh(String name, int[] indices, float[] vertices) {
+            this(name, indices, vertices, null, null);
+        }
+
+        public OBJMesh(String name, int[] indices, float[] vertices, float[] normals, float[] textureCoords) {
             this.name = name;
             this.indices = indices;
             this.vertices = vertices;
+            this.normals = normals;
+            this.textureCoords = textureCoords;
         }
+
     }
 
     public static String writeMultiple(List<OBJMesh> meshes) {
         StringBuilder str = new StringBuilder();
         int vertexCount = 1;
+        int uvCount = 1;
 
         for (OBJMesh objMesh : meshes) {
             str.append("o ").append(objMesh.name).append("\n");
@@ -82,18 +92,42 @@ public class OBJExporter {
                         .append(objMesh.vertices[i + 2]).append("\n");
             }
 
+            if (objMesh.normals != null) {
+                // Normals
+                str.append("# NORMALS \n");
+                for (int i = 0; i < objMesh.normals.length; i += 3) {
+                    str.append("vn ").append(objMesh.normals[i]).append(" ")
+                            .append(objMesh.normals[i + 1]).append(" ")
+                            .append(objMesh.normals[i + 2]).append("\n");
+                }
+            }
+
+            if (objMesh.textureCoords != null) {
+                // Texture coords
+                str.append("# UV \n");
+                for (int i = 0; i < objMesh.textureCoords.length; i += 2) {
+                    str.append("vt ").append(objMesh.textureCoords[i]).append(" ")
+                            .append(1.0f - objMesh.textureCoords[i + 1]).append("\n");
+                }
+            }
+
             // Indices
             str.append("# FACES \n");
             for (int i = 0; i < objMesh.indices.length; i += 3) {
                 int v1 = objMesh.indices[i] + vertexCount;
+                int uv1 = objMesh.indices[i] + uvCount;
                 int v2 = objMesh.indices[i + 1] + vertexCount;
+                int uv2 = objMesh.indices[i + 1] + uvCount;
                 int v3 = objMesh.indices[i + 2] + vertexCount;
+                int uv3 = objMesh.indices[i + 2] + uvCount;
 
-                String face = "f " + v1 + " " + v2 + " " + v3;
+                String face = "f " + v1 + "/" + uv1 + " " + v2 + "/" + uv2 + " " + v3 + "/" + uv3;
                 str.append(face).append("\n");
             }
+            str.append("\n");
 
             vertexCount += objMesh.vertices.length / 3;
+            uvCount += objMesh.textureCoords.length / 2;
         }
 
         return str.toString();
